dfc8c8ce9e42c6829a83e4ae268104b846e0057a
Luis Esturbán <46576209+Est17256@users.noreply.github.com>
2020-04-18 16:23:33 -0600

    3-Way Handshake Cliente

--- cliente/Consola/ClienteFinal.cpp
+++ cliente/Consola/ClienteFinal.cpp
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "mensaje.pb.h"
 
 #include <iostream>
 #include <string>
@@ -17,6 +18,8 @@
 #include <vector>
 
 using namespace std;
+using namespace chat;
+using namespace google::protobuf;
 
 #define DELAY 30000
 #define BUFSIZE 1024
@@ -366,48 +369,123 @@ void error(const char *msg)
 }
 
 int main(int argc, char *argv[]) {
-	/////////////////////////////////////////////////////////////////
-    /*
-	int sockfd, portno, n;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-	bool salir = false;
-	char buffer[BUFSIZE];
 
-	if (argc < 3) {
-	   fprintf(stderr,"usage %s hostname port\n", argv[0]);
-	   exit(0);
-	}
+    int sockfd, portno, n, userId;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    bool salir = false;
+    char buffer[BUFSIZE];
 
-	portno = atoi(argv[3]);
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-	if (sockfd < 0) 
-		error("ERROR al abrir socket");
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
 
-	
-	// Obtener host desde consola
-	server = gethostbyname(argv[2]);
+    portno = atoi(argv[3]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (server == NULL) {
-		fprintf(stderr,"ERROR, no existe la dirección del host\n");
-		exit(0);
-	}
+    if (sockfd < 0) 
+        error("ERROR al abrir socket");
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
+    
+    // Obtener host desde consola
+    server = gethostbyname(argv[2]);
 
-	bcopy((char *)server -> h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no existe la dirección del host\n");
+        exit(0);
+    }
 
-	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-		error("ERROR, no se pudo conectar al servidor");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
 
-	printf("Esperando confirmación del servidor...\n");
-	recv(sockfd, buffer, BUFSIZE, 0);
-	printf("Conexión aceptada!\n\n");
-    */
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+    bcopy((char *)server -> h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR, no se pudo conectar al servidor");
+
+    /* THREE-WAY-HANDSHAKE */
+
+
+
+
+
+    //          1. SOLICITAR SINCRONIZACION DE INFO
+
+    // Se crea instacia tipo MyInfoSynchronize y se setean los valores deseables
+    MyInfoSynchronize * miInfo(new MyInfoSynchronize);
+    miInfo->set_username(argv[1]);
+    miInfo->set_ip(argv[4]);
+
+    // Se crea instancia de Mensaje, se setea los valores deseados
+    ClientMessage m;
+    m.set_option(1);
+    m.set_allocated_synchronize(miInfo);
+
+    // Se serializa el message a string
+    string binary;
+    m.SerializeToString(&binary);
+
+    char cstr[binary.size() + 1];
+    strcpy(cstr, binary.c_str());
+
+    send(sockfd, cstr, strlen(cstr), 0);
+
+    printf("Esperando confirmación del servidor...\n");
+
+    //          2. ESPERAR RESPUESTA DE SINCRONIZACIÓN
+
+    // Recibir mensajes del cliente conectado
+    bzero(buffer, BUFSIZE);
+    recv(sockfd, buffer, BUFSIZE, 0);
+
+    if (1)
+    {
+
+        string ret(buffer, BUFSIZE);
+    }
+    ServerMessage sm;
+    sm.ParseFromString(buffer);
+    int option = sm.option();
+
+    // MANEJAR MY INFO RESPONSE
+    if (option == 4)
+    {
+        userId = sm.myinforesponse().userid();
+        cout << "MY ID ES: " << userId << endl;
+
+
+        //          3. HACER SABER AL SERVIDOR QUE ESTAMOS LISTOS
+
+        // Se crea instancia tipo MyInfoAcknowledge
+        MyInfoAcknowledge * ack(new MyInfoAcknowledge);
+        ack->set_userid(userId);
+
+        // Se crea instancia de Mensaje, se setea los valores deseados
+        ClientMessage m2;
+        m2.set_option(6);
+        m2.set_allocated_acknowledge(ack);
+
+        // Se serializa el message a string
+        string binary2;
+        m2.SerializeToString(&binary2);
+
+        char cstr2[binary2.size() + 1];
+        strcpy(cstr2, binary2.c_str());
+
+        send(sockfd, cstr2, strlen(cstr2), 0);
+
+    }
+
+///////////////////////////////////////////////
+
+
+
+
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~
 //~~ Cosas Temporales ~~~~
